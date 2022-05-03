8b514a82639e84dbd571cb8f9a43ebf3908c2ad5
DarriusB <darrius.bradford@gmail.com>
2022-05-03 09:24:31 -0700

    Exercise is done

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        private double balance = 0;
+
+
+        public BankAccount()
+        {
+
+        }
+
+
+        public void Deposit(double value)
+        {
+            balance += value;
+
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var bankAccount = new BankAccount();
+            Console.Write("How much would you like to deposit? :$");
+            double amount = double.Parse(Console.ReadLine());
+            bankAccount.Deposit(amount);
+            Console.WriteLine($"Your new balance is: {bankAccount.GetBalance()}");
+
+
+        }
+
+
+
+    }
+}
